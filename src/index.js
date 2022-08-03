25063cc44eb1d238e63acc975084767060bbd8ce
Pavel Didenko <win4ester2016@yandex.ru>
2022-08-03 22:55:12 +0300

    The task is ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
-}
+  number = String(n);
+  result = '';
+  for (let i = number.length - 1; i >= 0; i-- ){
+    if (number[i] != '-'){
+    result += number[i];
+    }
+  }
+  return result;
+  }
+
